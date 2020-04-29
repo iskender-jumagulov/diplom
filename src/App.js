5bc8481438885c73746e6b13c01a62e957439c1a
Iskender <jiskender1@gmail.com>
2020-04-29 11:18:28 +0600

    ..

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import Layout from './components/Layout/Layout';
+import Layout from "./components/Layout/Layout";
+import StationeryBuilder from './containers/Bag Builder/BagBuilder';
 import './App.css';
 
 export default () => {
   return (
     <div className="App">
       <Layout>
-       
+        <BagBuilder />
       </Layout>
     </div>
   );
 };
-
-
 
